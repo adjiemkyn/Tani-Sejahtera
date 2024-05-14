5bf89ba0343c732bbacc366774e2949f7655f506
Suastiniputu <psuastini28@gmail.com>
2024-05-14 19:56:45 +0800

    MCA test

--- include/secret.h
+++ include/secret.h
@@ -1,5 +1,5 @@
-const char* ssid = "Ingat Tuhan";
-const char* pass = "bukanbudakai";
+const char* ssid = "LAB TI";
+const char* pass = "#tiundiknas";
 const char* id = "com.TaniSejahtera.MicroclimateConditionAgent";
 const char* broker = "broker.hivemq.com";
 const char* brokerUsername = "public";
