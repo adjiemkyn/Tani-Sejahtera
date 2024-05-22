f32164a69f9256f23fba99bf41d3a85e1c901e40
adjiemkyn <adjiemikayana@gmail.com>
2024-05-22 17:09:27 +0800

    first

---
+++ HumanComputerInteractionAgent/.pio/build/esp32doit-devkit-v1/lib782/MQTT/lwmqtt/string.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32doit-devkit-v1/lib782/MQTT/lwmqtt/string.c.o: \
+ .pio/libdeps/esp32doit-devkit-v1/MQTT/src/lwmqtt/string.c \
+ .pio/libdeps/esp32doit-devkit-v1/MQTT/src/lwmqtt/lwmqtt.h
---
+++ HumanComputerInteractionAgent/src/secret.h
@@ -0,0 +1,7 @@
+const char *ssid = "Adjie";
+const char *pass = "29102000";
+const char *id = "com.tanisejahtera.humancomputerinteractionagent";
+const char *broker = "broker.hivemq.com";
+const char *brokerUsername = "public";
+const char *brokerPassword = "public";
+const int port = 1883;
