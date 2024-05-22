843023a74b50f44de50e87d236a3eb0bc3aa5c79
adjiemkyn <96862666+adjiemkyn@users.noreply.github.com>
2024-05-22 17:19:51 +0800

    lib

--- HumanComputerInteractionAgent/.pio/build/esp32doit-devkit-v1/lib782/MQTT/lwmqtt/client.c.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32doit-devkit-v1/lib782/MQTT/lwmqtt/client.c.o: \
- .pio/libdeps/esp32doit-devkit-v1/MQTT/src/lwmqtt/client.c \
- .pio/libdeps/esp32doit-devkit-v1/MQTT/src/lwmqtt/packet.h \
- .pio/libdeps/esp32doit-devkit-v1/MQTT/src/lwmqtt/helpers.h \
- .pio/libdeps/esp32doit-devkit-v1/MQTT/src/lwmqtt/lwmqtt.h
--- HumanComputerInteractionAgent/.pio/build/esp32doit-devkit-v1/lib782/MQTT/lwmqtt/packet.c.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32doit-devkit-v1/lib782/MQTT/lwmqtt/packet.c.o: \
- .pio/libdeps/esp32doit-devkit-v1/MQTT/src/lwmqtt/packet.c \
- .pio/libdeps/esp32doit-devkit-v1/MQTT/src/lwmqtt/packet.h \
- .pio/libdeps/esp32doit-devkit-v1/MQTT/src/lwmqtt/helpers.h \
- .pio/libdeps/esp32doit-devkit-v1/MQTT/src/lwmqtt/lwmqtt.h
