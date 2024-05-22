79d948314a2a0333b2d8d3ad27e6b450512311b1
adjiemkyn <adjiemikayana@gmail.com>
2024-05-22 20:14:01 +0800

    aktuator lingkungan

--- MicroclimateAdjusterAgent/lib/UrusanAktuatorLingkungan/src/UrusanAktuatorLingkungan.h
+++ MicroclimateAdjusterAgent/lib/UrusanAktuatorLingkungan/src/UrusanAktuatorLingkungan.h
@@ -3,23 +3,24 @@
 
 #include "Arduino.h"
 
-class UrusanAktuatorLingkungan {
-    public:
-        UrusanAktuatorLingkungan(uint8_t ina, uint8_t inb);
-        void mulai();
-        void nyalakan(uint8_t kekuatan, bool arah);
-        void padamkan();
-        float bacaKekuatan();
-        bool bacaArah();
-        bool bacaStatus();
-        void anginTopan(bool arah);
-    
-    private:
-        uint8_t _ina;
-        uint8_t _inb;
-        float _kekuatan;
-        bool _arah;
-        bool _status;
+class UrusanAktuatorLingkungan
+{
+public:
+    UrusanAktuatorLingkungan(uint8_t ina, uint8_t inb);
+    void mulai();
+    void nyalakan(uint8_t kekuatan, bool arah);
+    void padamkan();
+    float bacaKekuatan();
+    bool bacaArah();
+    bool bacaStatus();
+    void anginTopan(bool arah);
+
+private:
+    uint8_t _ina;
+    uint8_t _inb;
+    float _kekuatan;
+    bool _arah;
+    bool _status;
 };
 
 #endif
