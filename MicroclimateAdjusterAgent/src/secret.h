0df0e7bc65ef33123b77039a06bf4eb05ab44320
adjiemkyn <adjiemikayana@gmail.com>
2024-05-28 19:04:51 +0800

    web

---
+++ MicroclimateAdjusterAgent/src/secret.h
@@ -0,0 +1,7 @@
+const char *ssid = "LAB TI";
+const char *pass = "#tiundiknas";
+const char *id = "com.TaniSejahtera.MicroclimateAdjusterAgent";
+const char *broker = "broker.hivemq.com";
+const char *brokerUsername = "public";
+const char *brokerPassword = "public";
+const int port = 1883;
